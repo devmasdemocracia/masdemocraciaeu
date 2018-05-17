406f6283756a2fcb991a5132fb16da1f73ae1ced
Senén Rodero Rodríguez <senenrodero@gmail.com>
2018-05-17 14:50:45 +0200

    Allow to deploy any branch when given as parameter

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :deploy_to, deploysecret(:deploy_to)
 set :server_name, deploysecret(:server_name)
 set :db_server, deploysecret(:db_server)
-set :branch, "masdemocracia-production"
+set :branch, ENV['branch'] || "masdemocracia-production"
 set :ssh_options, port: deploysecret(:ssh_port)
 set :stage, :production
 set :rails_env, :production
