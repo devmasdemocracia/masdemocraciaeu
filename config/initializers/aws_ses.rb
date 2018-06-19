48f007729a3c69f255b7033b9e7c8b2181efc2f8
Senén Rodero Rodríguez <senenrodero@gmail.com>
2018-06-19 15:17:58 +0200

    Remove aws-ses gem

--- config/initializers/aws_ses.rb
+++
@@ -1,3 +0,0 @@
-ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  access_key_id:     Rails.application.secrets.ses_access_key_id,
-  secret_access_key: Rails.application.secrets.ses_secret_access_key
