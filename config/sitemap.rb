dc24166d49076a6f8827d7213d26f9543a3ca02c
Senén Rodero Rodríguez <senenrodero@gmail.com>
2018-07-21 10:56:29 +0200

    Custom MasDemocraciaEnEuropa sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -10,38 +10,48 @@ SitemapGenerator::Sitemap.default_host = Setting["url"]
 
 # sitemap generator
 SitemapGenerator::Sitemap.create do
-  pages = ["general_terms"]
+  pages = ["privacy", "conditions", "accesibility", "legal_disclaimer", "proposal_guide"]
   pages.each do |page|
     add page_path(id: page)
   end
 
   add help_path
-  add how_to_use_path
-  add faq_path
+  add how_to_use_path if Setting['org_name'] != 'MASDEMOCRACIAEUROPA'
+  add faq_path if Setting['org_name'] != 'MASDEMOCRACIAEUROPA'
 
-  add debates_path, priority: 0.7, changefreq: "daily"
-  Debate.find_each do |debate|
-    add debate_path(debate), lastmod: debate.updated_at
+  if Setting['feature.debates']
+    add debates_path, priority: 0.7, changefreq: "daily"
+    Debate.find_each do |debate|
+      add debate_path(debate), lastmod: debate.updated_at
+    end
   end
 
-  add proposals_path, priority: 0.7, changefreq: "daily"
-  Proposal.find_each do |proposal|
-    add proposal_path(proposal), lastmod: proposal.updated_at
+  if Setting['feature.proposals']
+    add proposals_path, priority: 0.7, changefreq: "daily"
+    Proposal.find_each do |proposal|
+      add proposal_path(proposal), lastmod: proposal.updated_at
+    end
   end
 
-  add budgets_path, priority: 0.7, changefreq: "daily"
-  Budget.find_each do |budget|
-    add budget_path(budget), lastmod: budget.updated_at
+  if Setting['feature.budgets']
+    add budgets_path, priority: 0.7, changefreq: "daily"
+    Budget.find_each do |budget|
+      add budget_path(budget), lastmod: budget.updated_at
+    end
   end
 
-  add polls_path, priority: 0.7, changefreq: "daily"
-  Poll.find_each do |poll|
-    add poll_path(poll), lastmod: poll.starts_at
+  if Setting['feature.polls']
+    add polls_path, priority: 0.7, changefreq: "daily"
+    Poll.find_each do |poll|
+      add poll_path(poll), lastmod: poll.starts_at
+    end
   end
 
-  add legislation_processes_path, priority: 0.7, changefreq: "daily"
-  Legislation::Process.find_each do |process|
-    add legislation_process_path(process), lastmod: process.start_date
+  if Setting['feature.legislation']
+    add legislation_processes_path, priority: 0.7, changefreq: "daily"
+    Legislation::Process.find_each do |process|
+      add legislation_process_path(process), lastmod: process.start_date
+    end
   end
 
 end
