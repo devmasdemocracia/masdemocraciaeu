f4b24c2af4fbbd207e6c10fd28b8805f325f32b2
Senén Rodero Rodríguez <senenrodero@gmail.com>
2018-07-21 12:36:32 +0200

    Fix spec

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -11,6 +11,7 @@ SitemapGenerator::Sitemap.default_host = Setting["url"]
 # sitemap generator
 SitemapGenerator::Sitemap.create do
   pages = ["aviso-legal", "de-que-se-trata", "accesibility", "politica-de-privacidad", "proposal_guide", "terminos-y-condiciones"]
+  pages << 'general_terms' if Setting['org_name'] != "MASDEMOCRACIAEUROPA"
   pages.each do |page|
     add page_path(id: page)
   end
